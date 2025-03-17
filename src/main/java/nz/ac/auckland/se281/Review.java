2a4c53ef46249c61d4f937db5e85757575cc41ab
Crynner <calvin.shih19@gmail.com>
2025-03-17 16:57:24 +1300

    added basic constructor for class Review

--- src/main/java/nz/ac/auckland/se281/Review.java
+++ src/main/java/nz/ac/auckland/se281/Review.java
@@ -5,4 +5,15 @@ abstract class Review {
   private int reviewRating;
   private String reviewText;
   private boolean reviewRecommended;
+
+  Review(String[] reviewDetails) {
+    this.reviewName = reviewDetails[0];
+    this.reviewRating = Integer.parseInt(reviewDetails[1]);
+    this.reviewText = reviewDetails[2];
+    if (reviewDetails[3].equals("y")) {
+      this.reviewRecommended = true;
+    } else {
+      this.reviewRecommended = false;
+    }
+  }
 }
